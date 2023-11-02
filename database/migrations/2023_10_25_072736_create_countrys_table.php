4203e85b342b848662faaba67fb71a194ccd51ab
akari27 <mhn12ka@gmail.com>
2023-11-02 18:20:34 +0900

    マイグレーションとシーディング

---
+++ database/migrations/2023_10_25_072736_create_countrys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countrys', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('geo_division_id')->constrained();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countrys');
+    }
+};
---
+++ database/migrations/2023_10_27_065947_create_foreign_visitors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foreign_visitors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('stay_location_id')->constrained('japan_locations','id');
+            $table->date('start_of_stay');
+            $table->date('end_of_stay');
+            $table->text('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foreign_visitors');
+    }
+};
