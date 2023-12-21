47d8d7a490f6d0c3b2d1ff00ba7a0cf73d37ad3e
akari27 <mhn12ka@gmail.com>
2023-12-21 12:12:53 +0900

    個別チャット画面のデザイン修正

--- app/Http/Controllers/Japanese/ChatController.php
+++ app/Http/Controllers/Japanese/ChatController.php
@@ -10,29 +10,34 @@ use App\Http\Controllers\Controller;
 use App\Models\Application;
 use App\Models\User;
 use App\Models\Chat;
+use App\Models\Country;
 use Illuminate\Support\Facades\Redirect;
 
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    public function index(User $user, Application $application)
+    public function index(User $user, Application $application, Country $country)
     {
         $approvedApplications = $application->where(function($query) {
             $query->where('sender_id', Auth::id())
                   ->orWhere('receiver_id', Auth::id());
         })->where('permission_flag', 1)->get();
         
-        $approvedApplications->each(function ($approved) use($user){
+        $approvedApplications->each(function ($approved) use($user, $country){
             if($approved->sender_id==Auth::id()){
                 $userId=$approved->receiver_id;
             }
             else{
                 $userId=$approved->sender_id;
             }
-            $approved->user_id=$user->with('japanese')->where('id',$userId)->first()->id;
-            $approved->name=$user->with('japanese')->where('id',$userId)->first()->name;
-            $approved->image_url=$user->with('japanese')->where('id',$userId)->first()->image_url;
+            $approved->add_info = $user->with('foreign_visitor')->where('id', $userId)->first();
+            $register_location = $country->where('id', $approved->add_info->foreign_visitor->register_location_id)->first();
+            if($register_location){
+                $approved->register_location = $register_location->name;
+            }else{
+                $approved->register_location = '未登録';
+            }
         });
         
         return Inertia::render('Chat/FriendsList',[
@@ -40,8 +45,15 @@ class ChatController extends Controller
         ]);
     }
     
-    public function show(User $user, Chat $chat)
+    public function show(User $user, Chat $chat, Country $country)
     {
+        $register_location = $country->where('id', $user->register_location_id)->first();
+        if($register_location){
+            $user->register_location = $register_location->name;
+        }else{
+            $user->register_location = '未登録';
+        }
+        
         $user_id = $user->id;
         $messages = $chat->where(function ($query) use ($user_id) {
                         $query->where('sender_id', Auth::id())->where('receiver_id', $user_id)
@@ -49,6 +61,13 @@ class ChatController extends Controller
                         $query->where('sender_id', $user_id)->where('receiver_id', Auth::id());
                     });
         })->get();
+        
+        $messages->each(function ($m)
+        {
+            $m->send_time = date('Y/m/d H:i', strtotime($m->created_at));
+        });
+        
+        // dd($user);
         return Inertia::render('Chat/Message',[
             'user'=>$user,
             'messages'=>$messages,
