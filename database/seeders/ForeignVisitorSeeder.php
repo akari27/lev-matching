b355381c3e42002113f87c5a8cfb85ce8e56ab38
akari27 <mhn12ka@gmail.com>
2024-01-18 17:03:31 +0900

    外国人のシーディング追加

--- database/seeders/ForeignVisitorSeeder.php
+++ database/seeders/ForeignVisitorSeeder.php
@@ -19,46 +19,76 @@ class ForeignVisitorSeeder extends Seeder
         // テスト用アカウント
         DB::table('foreign_visitors')->insert([
             'user_id' => '7',
+            // 韓国
             'register_location_id' => '7',
-            'stay_location_id' => '13',
-            'start_of_stay' => '2023-07-20',
-            'end_of_stay' => '2023-07-30',
+            // 東京か千葉にする
+            'stay_location_id' => '12',
+            'start_of_stay' => '2024-07-20',
+            'end_of_stay' => '2024-07-30',
             'reason' => '日本の音楽が好きで音楽フェスに行きたいから',
         ]);
         // 仮ユーザ
         DB::table('foreign_visitors')->insert([
             [
                 'user_id' => '8',
-                'register_location_id' => '2',
-                'stay_location_id' => '2',
-                'start_of_stay' => '2023-01-20',
-                'end_of_stay' => '2023-01-30',
-                'reason' => '観光したい',
+                // パキスタン
+                'register_location_id' => '10',
+                // 福岡
+                'stay_location_id' => '40',
+                'start_of_stay' => '2024-05-20',
+                'end_of_stay' => '2024-06-10',
+                'reason' => 'スポーツ観戦',
             ],
             [
                 'user_id' => '9',
-                'register_location_id' => '3',
-                'stay_location_id' => '3',
-                'start_of_stay' => '2023-01-20',
-                'end_of_stay' => '2023-01-30',
-                'reason' => '日本食をたのしみたい',
+                // 中国
+                'register_location_id' => '8',
+                // 北海道
+                'stay_location_id' => '1',
+                'start_of_stay' => '2024-08-15',
+                'end_of_stay' => '2024-09-05',
+                'reason' => '自然・リラックス',
             ],
             [
                 'user_id' => '10',
-                'register_location_id' => '1',
-                'stay_location_id' => '1',
-                'start_of_stay' => '2023-01-20',
-                'end_of_stay' => '2023-01-30',
-                'reason' => '日本の音楽が好きでライブに行きたいから',
+                // タイ
+                'register_location_id' => '6',
+                // 京都
+                'stay_location_id' => '26',
+                'start_of_stay' => '2024-04-10',
+                'end_of_stay' => '2024-04-30',
+                'reason' => 'グルメ巡り',
             ],
             [
                 'user_id' => '11',
-                'register_location_id' => '2',
-                'stay_location_id' => '2',
-                'start_of_stay' => '2023-01-20',
-                'end_of_stay' => '2023-01-30',
-                'reason' => '買い物したい',
-            ]
+                // 韓国
+                'register_location_id' => '7',
+                // 大阪
+                'stay_location_id' => '27',
+                'start_of_stay' => '2024-03-20',
+                'end_of_stay' => '2024-03-30',
+                'reason' => '東京観光・カフェ巡り・買い物',
+            ],
+            [
+                'user_id' => '12',
+                // インド
+                'register_location_id' => '1',
+                // 東京
+                'stay_location_id' => '13',
+                'start_of_stay' => '2024-01-20',
+                'end_of_stay' => '2024-01-30',
+                'reason' => '日本文化を知る',
+            ],
+            [
+                'user_id' => '13',
+                // ベトナム
+                'register_location_id' => '16',
+                // 東京
+                'stay_location_id' => '13',
+                'start_of_stay' => '2024-02-20',
+                'end_of_stay' => '2024-02-29',
+                'reason' => '日本のテクノロジーに触れる・買い物',
+            ],
         ]);
     }
 }
